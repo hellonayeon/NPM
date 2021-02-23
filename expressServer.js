e2da54d954f5288419596946095020fe9ed40156
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-23 16:34:20 +0900

    사용자 조회 기능 작성

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
- 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '19980815',
+  database : 'fintech'
+});
+connection.connect(); 
+//connection.end();
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/user', function (req, res) {
-    res.send('사용자 페이지 입니다.')
-  })
+    connection.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results[0])
+    });
+})
  
 app.listen(3000)
