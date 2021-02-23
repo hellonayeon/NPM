67e9596932c1dcfedf2943b26251c0669bd6977c
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-23 16:03:27 +0900

    데이터베이스 테이블 조회

--- databaseConnect.js
+++ databaseConnect.js
@@ -12,5 +12,10 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
+
+connection.query('SELECT * FROM user', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
  
 connection.end();
