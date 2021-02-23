1c7da8f255977e20c7255e3043a53d4d680b364e
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-23 16:45:40 +0900

    ejs 모듈 추가 및 ejs 샘플 파일 추가

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views'); // 뷰 파일이 있는 디렉토리를 설정
+app.set('view engine', 'ejs'); // 뷰 엔진으로 EJS 사용 선언
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+app.get('/user', function (req, res) {
+    connection.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results[0]);
+    });
+})
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -9,17 +25,5 @@ var connection = mysql.createConnection({
 });
 connection.connect(); 
 //connection.end();
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.get('/user', function (req, res) {
-    connection.query('SELECT * FROM user', function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-        res.send(results[0])
-    });
-})
  
-app.listen(3000)
+app.listen(3000);
