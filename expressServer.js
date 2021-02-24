06b7cde2c6349398a1b58ffcc34db8076df440b8
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-24 10:48:02 +0900

    사용자 요청 / 응답 코드 완성

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// json 타입의 데이터 전송을 허용
+app.use(express.json());
+
+// form 타입의 데이터 전송을 허용
+app.use(express.urlencoded({ extended:false }));
+
 app.set('views', __dirname + '/views'); // 뷰 파일이 있는 디렉토리를 설정
 app.set('view engine', 'ejs'); // 뷰 엔진으로 EJS 사용 선언
 
@@ -14,6 +20,8 @@ app.get('/ejs', function(req, res) {
 
 app.post('/userData', function(req, res) { // 사용자의 요청을 POST로 받는 라우터 생성
   console.log("사용자의 요청이 발생했습니다.")
+  console.log(req.body) // 서버에서 request body 를 허용한다는 선언 필요
+  res.send(true);
 }) 
 
 app.get('/user', function (req, res) {
