00265e5b9ffc0896c6bbc34580def6efa05240dc
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-24 11:11:16 +0900

    디자인 파일 추가 및 express publich 정적 자원 디렉토리 설정

--- expressServer.js
+++ expressServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path'); // 라이브러리 불러오기
 const app = express();
 
 // json 타입의 데이터 전송을 허용
@@ -6,6 +7,8 @@ app.use(express.json());
 
 // form 타입의 데이터 전송을 허용
 app.use(express.urlencoded({ extended:false }));
+
+app.use(express.static(path.join(__dirname, 'public'))); //to use static asset (정적 리소스 공개)
 
 app.set('views', __dirname + '/views'); // 뷰 파일이 있는 디렉토리를 설정
 app.set('view engine', 'ejs'); // 뷰 엔진으로 EJS 사용 선언
